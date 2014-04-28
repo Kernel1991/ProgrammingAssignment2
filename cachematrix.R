efe8a8bcd73d3da27f528f24c607dc9a4010e976
Kernel1991 <irek.akberov@gmail.com>
2014-04-27 22:30:12 -0400

    my homework changes

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,46 @@
 ## functions do
 
 ## Write a short comment describing this function
+## function that has four matrix and four functions inside
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## assigns m to be NULL
+    m <- NULL
+    
+    ## function that sets the matrix
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    ## function that displays gets the matrix
+    get <- function () x
+    
+    ## function that sets Inverse
+    setInverse <- function(d) {
+        m <<- d
+    }
+    
+    ## function that gets Inverse
+    getInverse <- function() m
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## function that check is the matrix is in cache, if not <- then computes
+## inverse and adds it to the original function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
+
